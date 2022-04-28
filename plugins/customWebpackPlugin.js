0b22870fda765fbada06f77e09b21009be9d4b8b
wangyicong5 <wangyicong5@jd.com>
2022-04-28 17:07:13 +0800

    feat(compiler hook): compiler钩子调试

--- plugins/customWebpackPlugin.js
+++ plugins/customWebpackPlugin.js
@@ -17,22 +17,101 @@ class CustomWebpackPlugin {
 
     // eslint-disable-next-line class-methods-use-this
     apply (compiler) {
-        compiler.hooks.emit.tapAsync('CustomWebpackPlugin', (compilation, callback) => {
-            const manifest = {};
-            for (const name of Object.keys(compilation.assets)) {
-                manifest[name] = compilation.assets[name].size();
-            // 将生成文件的文件名和大小写入manifest对象
-            }
-            compilation.assets['manifest.json'] = {
-                source () {
-                    return JSON.stringify(manifest);
-                },
-                size () {
-                    return this.source().length;
-                },
-            };
-            callback();
+        console.log('【初始化插件】=> start')
+        compiler.hooks.environment.tap('CustomWebpackPlugin', () => {
+            console.log('【阶段】environment =>')
+        })
+        compiler.hooks.afterEnvironment.tap('CustomWebpackPlugin', () => {
+            console.log('【阶段】afterEnvironment =>')
+        })
+        compiler.hooks.entryOption.tap('CustomWebpackPlugin', () => {
+            console.log('【阶段】entryOption =>')
+        })
+        compiler.hooks.afterPlugins.tap('CustomWebpackPlugin', () => {
+            console.log('【阶段】afterPlugins =>')
+        })
+        compiler.hooks.afterResolvers.tap('CustomWebpackPlugin', () => {
+            console.log('【阶段】afterResolvers =>')
+        })
+        compiler.hooks.initialize.tap('CustomWebpackPlugin', () => {
+            console.log('【阶段】initialize =>')
+        })
+        compiler.hooks.beforeRun.tap('CustomWebpackPlugin', () => {
+            console.log('【阶段】beforeRun =>')
+        })
+        compiler.hooks.run.tap('CustomWebpackPlugin', () => {
+            console.log('【阶段】run => ========================== ')
+        })
+        compiler.hooks.watchRun.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】watchRun =>')
         });
+        compiler.hooks.normalModuleFactory.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】normalModuleFactory =>')
+        });
+        compiler.hooks.contextModuleFactory.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】contextModuleFactory =>')
+        });
+        compiler.hooks.beforeCompile.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】beforeCompile =>')
+        });
+        compiler.hooks.compile.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】compile =>')
+        });
+        compiler.hooks.thisCompilation.tap('CustomWebpackPlugin', compilation => {
+            console.log('【阶段】thisCompilation =>')
+        });
+        compiler.hooks.compilation.tap('CustomWebpackPlugin', compilation => {
+            compilation.hooks.processAssets.tapPromise({
+                name: 'CustomWebpackPlugin',
+                stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_SIZE,
+                additionalAssets: true,
+            }, assets => {
+                console.log(assets)
+                return Promise.resolve();
+            });
+            console.log('【阶段】thisCompilation =>')
+        });
+        compiler.hooks.make.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】make => ========================== ')
+        });
+        compiler.hooks.afterCompile.tap('CustomWebpackPlugin', compilation => {
+            console.log('【阶段】afterCompile =>')
+        });
+        compiler.hooks.shouldEmit.tap('CustomWebpackPlugin', compilation => {
+            console.log('【阶段】shouldEmit =>')
+        })
+        compiler.hooks.emit.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】emit =>')
+        });
+        compiler.hooks.afterEmit.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】afterEmit =>')
+        });
+        compiler.hooks.assetEmitted.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】assetEmitted =>')
+        });
+        compiler.hooks.done.tap('CustomWebpackPlugin', stats => {
+            console.log('【阶段】done =>')
+        });
+        compiler.hooks.additionalPass.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】additionalPass =>')
+        });
+        compiler.hooks.failed.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】failed =>')
+        });
+        compiler.hooks.invalid.tap('CustomWebpackPlugin', () => {
+            console.log('【阶段】invalid =>')
+        });
+        compiler.hooks.watchClose.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】watchClose =>')
+        });
+        compiler.hooks.infrastructureLog.tap('CustomWebpackPlugin', (compilation, callback) => {
+            console.log('【阶段】infrastructureLog =>')
+        });
+        // 这个函数好像没有
+        // compiler.hooks.log.tap('CustomWebpackPlugin', (compilation, callback) => {
+        //     console.log('【阶段】log =>')
+        // });
+        console.log('【初始化插件】=> end')
     }
 }
 
