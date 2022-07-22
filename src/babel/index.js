1f3d799eda5cd0e5723cd69960ead9a859c1ec83
JuctTr <1301842163@qq.com>
2022-07-23 01:09:06 +0800

    feat(babel): babel插件为Tree Shaking做了什么事情？

--- src/babel/index.js
+++ src/babel/index.js
@@ -1,6 +1,24 @@
-import { greeting } from './utils';
+// import { greeting } from './utils';
 
-const square = n => n * n;
-console.log('node xxxxxxx')
+// const square = n => n * n;
+// console.log('node xxxxxxx')
 
-greeting()
+// greeting()
+
+const core = require('@babel/core');
+const babelPluginImport = require('./plugins/babel-plugin-import');
+
+const sourceCode = `
+  import { Button, Alert } from 'hy-store';
+`;
+
+const parseCode = core.transform(sourceCode, {
+    plugins: [
+        babelPluginImport({
+            libraryName: 'hy-store',
+        }),
+    ],
+});
+
+console.log(sourceCode, '输入的Code');
+console.log(parseCode.code, '输出的结果');
