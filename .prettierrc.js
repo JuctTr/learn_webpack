c98a5ae289b971c70e58346faf93ab7ba5086ff0
JuctTr <1301842163@qq.com>
2022-08-01 21:02:28 +0800

    feat(prettier): fix

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+// .prettierrc => .prettierrc.js 可以写注释
+
+module.exports = {
+    "tabWidth": 4,
+    "singleQuote": true,  // 单引号
+    "semi": true, // 末尾分号
+    "arrowParens": "avoid",
+    // printWidth: 100, // 代码宽度建议不超过100字符
+    jsxSingleQuote: true, // jsx中使用单引号
+    trailingComma: 'es5', // 尾随逗号
+    arrowParens: 'avoid', // 箭头函数仅在必要时使用()
+    htmlWhitespaceSensitivity: 'css', // html空格敏感度
+}
+
+
