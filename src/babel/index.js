956074fcd0427363cd57cdd99b6bee17ccf51fc8
JuctTr <1301842163@qq.com>
2022-12-02 14:16:04 +0800

    feat(🤡): none

--- src/babel/index.js
+++ src/babel/index.js
@@ -1,24 +1,39 @@
-// import { greeting } from './utils';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
-// const square = n => n * n;
-// console.log('node xxxxxxx')
+const arrowFunction = () => {
+    console.log('Hello My name is juctTr');
+};
 
-// greeting()
+arrowFunction();
 
-const core = require('@babel/core');
-const babelPluginImport = require('./plugins/babel-plugin-import');
+const arr = [1];
 
-const sourceCode = `
-  import { Button, Alert } from 'hy-store';
-`;
+const result = arr.includes(2);
 
-const parseCode = core.transform(sourceCode, {
-    plugins: [
-        babelPluginImport({
-            libraryName: 'hy-store',
-        }),
-    ],
+console.log(result, 'result');
+
+new Promise((resolve, reject) => {
+    console.log('我是 promise');
+    resolve('lalala');
+}).then(res => {
+    console.log(res);
 });
 
-console.log(sourceCode, '输入的Code');
-console.log(parseCode.code, '输出的结果');
+// const core = require('@babel/core');
+// const babelPluginImport = require('./plugins/babel-plugin-import');
+
+// const sourceCode = `
+//   import { Button, Alert } from 'hy-store';
+// `;
+
+// const parseCode = core.transform(sourceCode, {
+//     plugins: [
+//         babelPluginImport({
+//             libraryName: 'hy-store',
+//         }),
+//     ],
+// });
+
+// console.log(sourceCode, '输入的Code');
+// console.log(parseCode.code, '输出的结果');
