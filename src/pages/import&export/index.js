7614f439d7c6e87a38dab0fb2c958f04eedfdfc0
JuctTr <1301842163@qq.com>
2023-02-11 16:53:12 +0800

    feat(import): import & export

---
+++ src/pages/import&export/index.js
@@ -0,0 +1,16 @@
+import { print } from './num.js';
+
+print();
+
+function button() {
+    const button = document.createElement('button');
+    const text = document.createTextNode('click me');
+    button.appendChild(text);
+    button.onclick = () =>
+        import('./info.js').then(res => {
+            console.log(res.log);
+        });
+    return button;
+}
+
+document.body.appendChild(button());
